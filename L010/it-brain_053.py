b07fbd619f7019e85c4840178fec78a52d23fb2d
Oleksandr Kovalov <barsuk4@gmail.com>
2023-08-05 13:57:57 +0300

    Add uri commit

--- L010/it-brain_053.py
+++ L010/it-brain_053.py
@@ -5,3 +5,7 @@ try:
     print(spisok[index])
 except IndexError:
     print(f"Ви намагаєтеся отримати доступ до індексу {index}, але довжина списку лише {len(spisok)}.")
+
+    """
+    https://github.com/barsuk4/IT-Brain/commit/85ce0d82d7cf9830234a9c18e39ee5aaa395480b
+    """
